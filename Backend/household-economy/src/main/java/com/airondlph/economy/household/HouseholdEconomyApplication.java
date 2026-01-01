f161547b66a8f4b647664bd882768755316d9d1b
airondlph <52699504+adriandlph@users.noreply.github.com>
2026-01-01 20:25:19 +0100

    Add backend project base

---
+++ Backend/household-economy/src/main/java/com/airondlph/economy/household/HouseholdEconomyApplication.java
@@ -0,0 +1,13 @@
+package com.airondlph.economy.household;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdEconomyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdEconomyApplication.class, args);
+	}
+
+}
---
+++ Backend/household-economy/src/test/java/com/airondlph/economy/household/HouseholdEconomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airondlph.economy.household;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdEconomyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
