bd62b023d94577bc327f1adb3cc96cdcbbb75e4c
airondlph <52699504+adriandlph@users.noreply.github.com>
2026-01-05 19:06:17 +0100

    Adds some logs

--- Backend/household-economy/src/main/java/com/airondlph/economy/household/HouseholdEconomyApplication.java
+++ Backend/household-economy/src/main/java/com/airondlph/economy/household/HouseholdEconomyApplication.java
@@ -1,13 +1,18 @@
 package com.airondlph.economy.household;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class HouseholdEconomyApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting Household Economy application...");
 		SpringApplication.run(HouseholdEconomyApplication.class, args);
+		log.info("Application started!");
 	}
+
 
 }
