9210a7e373c21ea17c5bd22e90d74daddcc8a37b
airondlph <52699504+adriandlph@users.noreply.github.com>
2026-01-18 17:06:53 +0100

    Correction of delete user

--- Backend/household-economy/src/main/java/com/airondlph/economy/household/api/rest/UsersRESTController.java
+++ Backend/household-economy/src/main/java/com/airondlph/economy/household/api/rest/UsersRESTController.java
@@ -8,7 +8,9 @@ import com.airondlph.economy.household.controller.users.SecurityController;
 import com.airondlph.economy.household.controller.users.UsersController;
 import com.airondlph.economy.household.data.model.TokenVO;
 import com.airondlph.economy.household.data.model.UserVO;
+import com.airondlph.economy.household.api.rest.exception.SecurityException;
 import com.airondlph.economy.household.exception.ServerErrorException;
+import com.auth0.jwt.interfaces.Claim;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
@@ -21,9 +23,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 /**
  * @author adriandlph / airondlph
@@ -97,26 +100,33 @@ public class UsersRESTController {
             produces = APPLICATION_JSON_VALUE
     )
     public ResponseEntity<RestApiResult<Void>> deleteUser(@PathVariable("id") String id) {
-        Long loggedUserId = 1L; // TODO: update when logging system is done
-        Long userToDeleteId;
+        Long loggedUserId;
 
+        try {
+            String token = SecurityRESTController.getBearerTokenHeader();
+            Map<String, Claim> claims = securityController.decodeToken(token);
+
+            Claim userIdClaim = claims.get("userId");
+            if (userIdClaim == null || userIdClaim.isMissing() || userIdClaim.isNull() || ((loggedUserId = userIdClaim.asLong()) == null)) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(RestApiResult.Error(2, "Not Authorized."));
+            }
+
+        } catch (ServerErrorException | SecurityException ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(RestApiResult.Error(-1, "Server error."));
+        }
+
+        Long userToDeleteId;
         try {
             userToDeleteId = Long.parseLong(id);
         } catch (Exception ex) {
             userToDeleteId = null;
         }
 
-        if (loggedUserId == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(RestApiResult.Error(2, "Not Authorized."));
-        }
-
         if ((userToDeleteId == null) || (userToDeleteId < 0) || (userToDeleteId >= Long.MAX_VALUE)) {
             return ResponseEntity.badRequest().body(RestApiResult.Error(3, "User not defined."));
         }
 
-
         Result<Void> createUserResult = usersController.deleteUserByIdVO(loggedUserId, userToDeleteId);
-
         if (!createUserResult.isValid()) {
             return switch (createUserResult.getErrCode()) {
                 case 2, 4 -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(RestApiResult.Error(2, "Not Authorized."));
